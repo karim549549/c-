758e42c95da18364989c01622335c063601f87b1
karim549549 <113533406+karim549549@users.noreply.github.com>
2022-09-23 14:02:47 +0200

    Update classvalidation.h

--- Route project/ConsoleApplication1/classvalidation.h
+++ Route project/ConsoleApplication1/classvalidation.h
@@ -4,11 +4,19 @@ using namespace std;
 
 class validation {
 public:
-	bool validname(string name){
-		if (size(name) > 4 && size(name) < 21)
-			return true;
-		else
-			return false;
+	bool validname(string name) {
+		bool truename = true;
+		if (size(name) > 4 && size(name) < 21) {
+			for (int i = 0;i <= size(name);i++) {
+				if ((name[i] >= 'A' && name[i] <= 'Z') || (name[i] >= 'a' && name[i] <= 'z'))
+					truename = true;
+				else {
+					truename = false;
+					break;
+				}
+		}
+	}
+		return truename;
 	}
 	bool validpass(string pass) {
 		if (size(pass) > 7 && size(pass) < 21)
@@ -28,5 +36,4 @@ public:
 		else
 			return false;
 	}
-
 };
